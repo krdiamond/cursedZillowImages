f537fa73aebbd4b889da30f03a1a07e6eba6f93c
Kristina <kristina.diamond@kryoinc.com>
2021-10-27 21:14:37 -0400

    set up working jump scare

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div v-if="ghostIsActive" class="ghost">
+      <div class="text">HAPPY HALLOWEEN</div>
+      <img src="./assets/ghost.jpeg">
+    </div>
+
+    <div v-if="screamIsActive" >
+      <audio autoplay><source src="./assets/scream.mp3" type="audio/mp3"></audio>
+    </div>
+  
+    <div id="cursedImageSwiper" class="swiper height--100">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide"><img class="swiper-image" src="./assets/cursed-images/cursedImage02.jpeg"></div>
+            <div class="swiper-slide"><img class="swiper-image" src="./assets/cursed-images/cursedImage03.jpeg"></div>
+            <div class="swiper-slide"><img class="swiper-image" src="./assets/cursed-images/cursedImage04.jpeg"></div>
+            <div class="swiper-slide"><img class="swiper-image" src="./assets/cursed-images/cursedImage05.jpeg"></div>
+            <div class="swiper-slide"><img class="swiper-image" src="./assets/cursed-images/cursedImage06.jpeg"></div>
+            <div class="swiper-slide"><img class="swiper-image" src="./assets/cursed-images/cursedImage07.jpeg"></div>
+            <div class="swiper-slide"><img class="swiper-image" src="./assets/cursed-images/cursedImage08.jpeg"></div>
+            <div class="swiper-slide"><img class="swiper-image" src="./assets/cursed-images/cursedImage09.jpeg"></div>
+            <div class="swiper-slide"><img class="swiper-image" src="./assets/cursed-images/cursedImage10.jpeg"></div>
+            <div class="swiper-slide"><img class="swiper-image" src="./assets/cursed-images/cursedImage11.jpeg"></div>
+            <div class="swiper-slide"><img class="swiper-image" src="./assets/cursed-images/cursedImage12.jpeg"></div>
+        </div>
+
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+
+    </div>
+
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  data(){
+    return {
+      ghostIsActive: false,
+      screamIsActive: false,
+    }
+  },
+  mounted() {
+    setTimeout(this.deployGhost, 25000)
+    setTimeout(this.deployScream, 24500)
+  },
+  methods: {
+    deployGhost(){
+      this.ghostIsActive = true;
+      console.log('BOO!')
+    },
+    deployScream(){
+      this.screamIsActive = true;
+      console.log('screaming!')
+    },
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -2,16 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 createApp(App).mount('#app')
 
-import Swiper, { Navigation, Pagination } from 'swiper';
+import './assets/style.scss';
+
+import Swiper, { Navigation } from 'swiper';
 import 'swiper/swiper.scss';
 import 'swiper/modules/navigation/navigation.scss';
-import 'swiper/modules/pagination/pagination.scss';
 
-Swiper.use([Navigation, Pagination]);
+Swiper.use([Navigation]);
 
-const testSwiper = document.getElementById('testSwiper');
+const cursedImageSwiper = document.getElementById('cursedImageSwiper');
 
-new Swiper(testSwiper, {
+new Swiper(cursedImageSwiper, {
     loop: true, 
     navigation: {
         nextEl: '.swiper-button-next',
